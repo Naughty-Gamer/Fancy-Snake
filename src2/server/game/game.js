afc46e857b55ebaf1de930361fbf7685142de5ce
Farhan Alvi <farhanalvi005@gmail.com>
2020-11-02 10:18:52 +0600

    Modularised server-side code

---
+++ src2/server/game/game.js
@@ -0,0 +1,30 @@
+
+class Game {
+    
+    constructor(socket_list){
+
+        setInterval(function(){
+
+            let snakes = []
+            for (let socket_id in socket_list){ 
+                let socket = socket_list[socket_id]
+                let snake = socket.snake
+                snake.body[0][1] -= 1 //testing
+                snakes.push(
+                    {
+                        snake: snake
+                    }
+                )
+            }
+
+            for (let socket_id in socket_list) {
+                let socket = socket_list[socket_id]
+                socket.emit('new_pos', snakes)
+            }
+
+        },1000)
+
+    }
+}
+
+module.exports = Game
--- src2/server/gameState.js
+++ src2/server/gameState.js
@@ -1,55 +1,70 @@
 const Snake = require("./game/snake.js")
+const Game = require("./game/game.js")
 
-let socket_list = {} // associative array id:<Socket>
+/**
+ * Object that serves as an associative array
+ * @type {"Socket.id : Socket"}
+ */
+let socket_list = {}
 
 /**
  * Manages the state of the game
- * @param {SocketIO.Server} io the server whose state will be managed
+ * @param {SocketIO.Server} server the websocket server whose sockets will be used
  */
-function manageState(io){
-    
+function manageState(server){
+
     // Event listener for every time someone joins the websocket connection
-    io.sockets.on('connection', function (clientSocket){
+    server.sockets.on('connection', function (clientSocket){
+
+        let newgame = null
+
+        if (Object.keys(socket_list).length == 0) {
+            newgame = getNewGame()
+        }
         
-        // Print to the server's terminal that a player connected
-        console.log("Player with ID:",clientSocket.id,"connected")
-        
-        clientSocket.emit('CONN_ACK',"You succesfully connected")
-        
-        let my_snake = new Snake(20,74)
-        clientSocket.snake = my_snake
-        
-        socket_list[clientSocket.id] = clientSocket // adding each socket connection to an associative array
+        initPlayer(clientSocket)
         
         clientSocket.on('disconnect',function(){
+            
+            terminatePlayer(clientSocket)
 
-            // Print to the server's terminal that a user disconnected
-            console.log("Player with ID:",clientSocket.id,"disconnected")
-            delete socket_list[clientSocket.id]
+            if (Object.keys(socket_list).length == 0) {
+                terminateGame(server,newgame)
+            }
         })
     })
 }
 
-setInterval(function(){
+function terminateGame(server, game){
+    delete game
+    server.close()
+    console.log("\nWebsocket server closed\n")
+}
 
-    let pack = []
-    for (let socket_index in socket_list){ 
-        let socket = socket_list[socket_index]
-        let snake = socket.snake
-        snake.body[0][1] -= 1 //testing
-        pack.push(
-            {
-                snake: snake
-            }
-        )
-    }
+function getNewGame(){
+    return new Game(socket_list)
+}
 
-    for (let socket_index in socket_list) {
-        let socket = socket_list[socket_index]
-        socket.emit('new_pos', pack)
-    }
+function terminatePlayer(clientSocket){ //temporary name
+    
+    // Print to the server's terminal that a user disconnected
+    console.log("Player with ID:",clientSocket.id,"disconnected")
+    delete socket_list[clientSocket.id]
 
-},1000)
+}
 
+function initPlayer(clientSocket){ //temporary name
+
+    // Print to the server's terminal that a player connected
+    console.log("Player with ID:",clientSocket.id,"connected")
+        
+    clientSocket.emit('CONN_ACK',"You succesfully connected")
+    
+    let my_snake = new Snake(20,74)
+    clientSocket.snake = my_snake
+    
+    socket_list[clientSocket.id] = clientSocket // adding each socket connection to an associative array
+
+}
 
 exports.manageState = manageState
