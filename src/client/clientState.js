704e7355ef51201e61a0c4efde6f650cda902599
Farhan Alvi <farhanalvi005@gmail.com>
2020-11-02 10:21:12 +0600

    Renamed and removed files

--- src/client/clientState.js
+++
@@ -1,8 +0,0 @@
-/**
- * This file should send the server updates regarding it's state, eg:
- * - The player's snake position
- * - Whether it has collided
- * - Whether it has eaten
- * - If it's alive
- * etc...
- */
