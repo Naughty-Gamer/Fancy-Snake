d23221eecdaf0e9eefe41a1d0575015d5cf20787
Farhan Alvi <farhanalvi005@gmail.com>
2020-11-12 05:09:11 +0600

    Structure client game better

--- client/src/game.js
+++ client/src/game.js
@@ -23,62 +23,55 @@ export default class Game {
 
 		socket.on("CONN_ACK", (msg) => {
 			console.log(msg)
-			this.initGame()
-		})
-	}
+			this.startRendering()
 
-	/**
-	 * Initialises the game on the browser
-	 * @param {SocketIO.Socket} socket Used for communicating with the server
-	 */
-	initGame() {
-		socket.on("init", (data) => {
-			data.snakes.forEach((snake) => {
-				new Snake(snake)
+			socket.on("init", (data) => {
+				data.snakes.forEach((snake) => {
+					new Snake(snake)
+				})
+
+				// data.allfood.forEach((foodlocation) => {
+				// 	new AllFood(foodlocation)
+				// })
 			})
 
-			// data.allfood.forEach((foodlocation) => {
-			// 	new AllFood(foodlocation)
-			// })
-		})
-
-		socket.on("update", (data) => {
-			data.snakes.forEach((pack) => {
-				let server_snake = pack.snake
-				let client_snake = Snake.player_list[server_snake.socketid]
-				if (client_snake) {
-					if (server_snake.headLocation !== undefined) {
-						client_snake.body = server_snake.body // naive implementation -- could just send head and tail
+			socket.on("update", (data) => {
+				data.snakes.forEach((pack) => {
+					let server_snake = pack.snake
+					let client_snake = Snake.player_list[server_snake.socketid]
+					if (client_snake) {
+						if (server_snake.headLocation !== undefined) {
+							client_snake.body = server_snake.body // naive implementation -- could just send head and tail
+						}
 					}
-				}
+				})
+
+				// data.allfood.forEach((foodlocation) => {
+				// 	let pack = foodlocation
+				// })
 			})
 
-			// data.allfood.forEach((foodlocation) => {
-			// 	let pack = foodlocation
-			// })
-		})
-
-		socket.on("remove", (data) => {
-			data.snakes.forEach((snake) => {
-				delete Snake.player_list[snake.socketid]
+			socket.on("remove", (data) => {
+				data.snakes.forEach((snake) => {
+					delete Snake.player_list[snake.socketid]
+				})
 			})
-		})
 
-		this.render()
-
-		// naive implementation -- need to have state for food
-		socket.on("food", (data) => {
-			this.food = data.food
-			// Clears the map before drawing every frame
-			// document.getElementById("game-map").innerHTML = ""
-			// drawEverySnake(data.snakes)
+			// naive implementation -- need to have state for food
+			socket.on("food", (data) => {
+				this.food = data.food
+				// Clears the map before drawing every frame
+				// document.getElementById("game-map").innerHTML = ""
+				// drawEverySnake(data.snakes)
+			})
 		})
 	}
 
-	render() {
+	startRendering() {
 		const fps = 60
 		const delayBetweenFramesInMs = 1000 / fps
 		setInterval(() => {
+			// document.body.innerHTML = ""
 			drawScoreBoard(Snake.player_list)
 			document.getElementById("game-map").innerHTML = ""
 			drawEverySnake(Snake.player_list)
@@ -88,25 +81,25 @@ export default class Game {
 
 	keyDownHandler(e) {
 		var key = e.key || e.keyCode
-		if (e.key === 68 || e.key == "d" || e.key == "ArrowRight") {
+		if (key === 68 || key == "d" || key == "ArrowRight") {
 			//d
 			if (this.direction != "left") {
 				this.direction = "right"
 				socket.emit("keyDown", { dir: this.direction })
 			}
-		} else if (key === 83 || e.key == "s" || e.key == "ArrowDown") {
+		} else if (key === 83 || key == "s" || key == "ArrowDown") {
 			//s
 			if (this.direction != "up") {
 				this.direction = "down"
 				socket.emit("keyDown", { dir: this.direction })
 			}
-		} else if (key === 65 || e.key == "a" || e.key == "ArrowLeft") {
+		} else if (key === 65 || key == "a" || key == "ArrowLeft") {
 			//a
 			if (this.direction != "right") {
 				this.direction = "left"
 				socket.emit("keyDown", { dir: this.direction })
 			}
-		} else if (key === 87 || e.key == "w" || e.key == "ArrowUp") {
+		} else if (key === 87 || key == "w" || key == "ArrowUp") {
 			// w
 			if (this.direction != "down") {
 				this.direction = "up"
