3c07926e8f24903a70726028e8da923b03679114
mma37 <mma37@hw.ac.uk>
2020-11-14 20:31:53 +0400

    Added the login and the Initial screen.

--- views/src/index.js
+++ views/src/index.js
@@ -1,4 +1,29 @@
-import Game from "./game.js"
+import Game from "./game.js";
 
-// export default 
-new Game()
+const loginbtn = document.getElementById("loginbtn");
+const registerbtn = document.getElementById("registerbtn");
+const signinModal = document.getElementById("signinmodal");
+const menuModal = document.getElementById("menuModal");
+const joinGameBtn = document.getElementById("joinGameBtn");
+const leaderBoardBtn = document.getElementById("leaderBoardBtn");
+const gameMap = document.getElementById("game-map");
+const scoreBoard = document.getElementById("scoreboard");
+
+loginbtn.addEventListener("click", mainMenu);
+registerbtn.addEventListener("click", mainMenu);
+joinGameBtn.addEventListener("click", startGame);
+
+function mainMenu() {
+	signinModal.style.display = "None";
+	menuModal.style.display = "block";
+}
+
+function startGame() {
+	menuModal.style.display = "none";
+	gameMap.style.display = "grid";
+	scoreBoard.style.display = "block";
+	new Game();
+}
+
+//
+// export default
