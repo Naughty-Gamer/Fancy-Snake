bb6a2e875c670e7e7b044d895939c061c53f3064
mma37 <mma37@hw.ac.uk>
2020-11-17 09:10:25 +0400

    Minor Change to fix a bug, where it keeps onkillin

--- services/KillTimer.js
+++ services/KillTimer.js
@@ -1,41 +1,41 @@
-const MAX_SECONDS = 7; // Delay it 2 or 3 seconds because it starts immediatly after player >= max_players (Change it after finishing testing)
+const MAX_SECONDS = 7 // Delay it 2 or 3 seconds because it starts immediatly after player >= max_players (Change it after finishing testing)
 
-let secondsUntilDeath = MAX_SECONDS;
-let timeInSeconds = 0;
+let secondsUntilDeath = MAX_SECONDS
+let timeInSeconds = 0
 
 function startKillTimer(snakes) {
 	let killTimerID = setInterval(() => {
-		timeInSeconds++;
-		secondsUntilDeath--;
-		let snakeSizes = {};
-		let snakeSizesArray = [];
+		timeInSeconds++
+		secondsUntilDeath--
+		let snakeSizes = {}
+		let snakeSizesArray = []
 
 		for (const socketid in snakes) {
-			let snake = snakes[socketid];
-			snakeSizes[socketid] = snake.tailIndex;
+			let snake = snakes[socketid]
+			snakeSizes[socketid] = snake.tailIndex
 			snakeSizesArray.push({
 				id: socketid,
 				length: snake.tailIndex,
-			});
+			})
 		}
-		let sortedSnakeSizesArray = snakeSizesArray.sort((a, b) => b.length - a.length);
-		console.log(sortedSnakeSizesArray, "\n");
-		let smallestPlayer = sortedSnakeSizesArray[sortedSnakeSizesArray.length - 1];
+		let sortedSnakeSizesArray = snakeSizesArray.sort((a, b) => b.length - a.length)
+		console.log(sortedSnakeSizesArray, "\n")
+		let smallestPlayer = sortedSnakeSizesArray[sortedSnakeSizesArray.length - 1]
 
 		if (secondsUntilDeath == 0) {
-			if (Object.keys(snakes).length !== 1) {
+			if (Object.keys(snakes).length > 1) {
 				// kills the smallest snake
-				snakes[smallestPlayer.id].die();
+				snakes[smallestPlayer.id].die()
 				// console.log(snakes[smallestPlayer.id], "got killed by the timer");
 			}
-			secondsUntilDeath = 5; //change it to 30 after finishing testing.
+			secondsUntilDeath = 5 //change it to 30 after finishing testing.
 		}
-	}, 1000);
+	}, 1000)
 
-	return killTimerID;
+	return killTimerID
 }
 
-module.exports.startKillTimer = startKillTimer;
+module.exports.startKillTimer = startKillTimer
 
 //Client side: when the countdown == 0, we will start drawing a clock or a timer.
 //Server side: when the countdown == 0, start the timerKill function.
