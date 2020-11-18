f95b5bf8bae8b29836a30cfea2f4587b6acfc158
mma37 <mma37@hw.ac.uk>
2020-11-19 00:02:09 +0400

    Fixed the rank system.

--- views/src/drawer.js
+++ views/src/drawer.js
@@ -1,27 +1,28 @@
-let gameMap = document.getElementById("game-map");
+let gameMap = document.getElementById("game-map")
 
-const x = 0;
-const y = 1;
+const x = 0
+const y = 1
+let rankNum = 1
 
 /**
  * Draws a snake on the map
  * @param {[number[]]} snake_body The body of the snake that will be drawn
  */
 function snakeDraw(snake_body) {
-	console.log(snake_body);
+	// console.log(snake_body);
 	snake_body.forEach((part) => {
-		const currentSnake = document.createElement("div"); // makes a div for our snake
-		currentSnake.style.gridRowStart = part[y]; // creates snake at part[1]
-		currentSnake.style.gridColumnStart = part[x]; // creates snake at part[0]
-		currentSnake.classList.add("snake"); // this adds the stylings to our snake div
-		gameMap.appendChild(currentSnake); // this adds a snake div as a child node
-	});
+		const currentSnake = document.createElement("div") // makes a div for our snake
+		currentSnake.style.gridRowStart = part[y] // creates snake at part[1]
+		currentSnake.style.gridColumnStart = part[x] // creates snake at part[0]
+		currentSnake.classList.add("snake") // this adds the stylings to our snake div
+		gameMap.appendChild(currentSnake) // this adds a snake div as a child node
+	})
 }
 
 export function drawEverySnake(snakes) {
 	for (const socketid in snakes) {
-		let snake_body = snakes[socketid].body;
-		snakeDraw(snake_body);
+		let snake_body = snakes[socketid].body
+		snakeDraw(snake_body)
 	}
 }
 
@@ -31,43 +32,51 @@ export function drawEverySnake(snakes) {
  */
 export function foodDraw(foodList) {
 	foodList.forEach((food) => {
-		const currentFood = document.createElement("div"); // makes a div for our food
-		currentFood.style.gridRowStart = food.foodLocation[y]; // creates food at getFoodLocation()[1]
-		currentFood.style.gridColumnStart = food.foodLocation[x]; // creates food at getFoodLocation()[0]
-		currentFood.classList.add("food"); // this adds the stylings to our food div
-		gameMap.appendChild(currentFood); // this adds a food div as a child node
-	});
+		const currentFood = document.createElement("div") // makes a div for our food
+		currentFood.style.gridRowStart = food.foodLocation[y] // creates food at getFoodLocation()[1]
+		currentFood.style.gridColumnStart = food.foodLocation[x] // creates food at getFoodLocation()[0]
+		currentFood.classList.add("food") // this adds the stylings to our food div
+		gameMap.appendChild(currentFood) // this adds a food div as a child node
+	})
 }
 
 export function drawScoreBoard(snakes) {
-	let rows = [];
-	let scoreboard = document.getElementById("scoreboard").querySelector("table");
+	let rows = []
+	let scoreboard = document.getElementById("scoreboard").querySelector("table")
 	// Add each row to the array
-	scoreboard.innerHTML = "";
-	let header = scoreboard.insertRow();
-	let hRank = header.insertCell(0);
-	hRank.innerHTML = "Rank";
-	let hUser = header.insertCell(1);
-	hUser.innerHTML = "Username";
-	let hLength = header.insertCell(2);
-	hLength.innerHTML = "Length";
+	scoreboard.innerHTML = ""
+	let header = scoreboard.insertRow()
+	let hRank = header.insertCell(0)
+	hRank.innerHTML = "Rank"
+	let hUser = header.insertCell(1)
+	hUser.innerHTML = "Username"
+	let hLength = header.insertCell(2)
+	hLength.innerHTML = "Length"
 	for (const socketid in snakes) {
-		let snake = snakes[socketid];
+		let snake = snakes[socketid]
 		rows.push({
 			username: snake.socketid,
 			score: snake.tailIndex,
-		});
+		})
 	}
 
-	let sortedPlayers = rows.sort((a, b) => b.score - a.score);
-
+	let sortedPlayers = rows.sort((a, b) => b.score - a.score)
 	sortedPlayers.forEach((player) => {
-		let row = scoreboard.insertRow();
-		let rank = row.insertCell(0);
-		rank.innerHTML = 0;
-		let username = row.insertCell(1);
-		username.innerHTML = player.username;
-		let length = row.insertCell(2);
-		length.innerHTML = player.score;
-	});
+		let row = scoreboard.insertRow()
+		let rank = row.insertCell(0)
+		let username = row.insertCell(1)
+		username.innerHTML = player.username
+		let length = row.insertCell(2)
+		length.innerHTML = player.score
+		getScore(sortedPlayers, player.score, rank)
+	})
+}
+
+function getScore(playerlist, score, rank) {
+	for (let index = 0; index < playerlist.length; index++) {
+		if (score == playerlist[index].score) {
+			rank.innerHTML = index + 1
+			return
+		}
+	}
 }
