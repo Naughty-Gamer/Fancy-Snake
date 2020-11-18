9a6641b42d902920e9f80e6cdef345c673515be2
mma37 <mma37@hw.ac.uk>
2020-11-19 00:03:03 +0400

    Added more css.

--- views/src/game.js
+++ views/src/game.js
@@ -1,124 +1,154 @@
-import { drawEverySnake, foodDraw, drawScoreBoard } from "./drawer.js";
-import Snake from "./models/snake.js";
+import { drawEverySnake, foodDraw, drawScoreBoard } from "./drawer.js"
+import Snake from "./models/snake.js"
 
-let socket = null;
+let socket = null
 
-Snake.player_list = {};
+Snake.player_list = {}
 
-const waitingText = document.getElementById("waiting-text");
-const waitingBox = document.getElementById("waiting-box");
-const gameMap = document.getElementById("game-map");
-let countdownText = "Waiting for players...";
+const waitingText = document.getElementById("waiting-text")
+const waitingBox = document.getElementById("waiting-box")
+const gameMap = document.getElementById("game-map")
+const container = document.getElementById("container")
+const back2menuBtn = document.getElementById("back2menuBtn")
+
+//still not implemented:
+const clock = document.getElementById("clock")
+const numbers = document.getElementById("numbers")
+
+let countdownText = "Waiting for players..."
 
 export default class Game {
 	constructor() {
-		this.direction = "";
+		this.snakeIsDead = false
+		this.direction = ""
 		// this.delay = 55
-		document.addEventListener("keydown", this.keyDownHandler);
+		document.addEventListener("keydown", this.keyDownHandler)
 		/**
 		 * Joins the websocket connection that the HTTP server has established.
 		 * More precisely, it connects the client to the default namespace `'/'`
 		 * - You can think of it as joining in on an on-going conference call
 		 */
-		socket = io();
+		socket = io()
 
 		socket.on("countdown", (data) => {
-			countdownText = data.time;
-		});
+			countdownText = data.time
+		})
+
+		socket.on("timetilldead", (data) => {
+			this.timeTillDeath = data.seconds
+			console.log("Until Death:", data.seconds)
+		})
 
 		socket.on("CONN_ACK", (confirmation) => {
-			console.log(confirmation);
-			this.startRendering();
-			this.registerListeners();
-		});
+			console.log(confirmation)
+			this.startRendering()
+			this.registerListeners()
+		})
 	}
 
 	registerListeners() {
 		socket.on("init", (data) => {
 			data.snakes.forEach((snake) => {
-				new Snake(snake);
-			});
-		});
+				new Snake(snake)
+			})
+		})
 
 		socket.on("update", (snakepack) => {
 			snakepack.snakes.forEach((server_snake) => {
-				let client_snake = Snake.player_list[server_snake.socketid];
+				let client_snake = Snake.player_list[server_snake.socketid]
 				if (client_snake) {
 					if (server_snake.headLocation !== undefined) {
 						// client_snake.updateBody(
 						// 	server_snake.headLocation,
 						// 	server_snake.tailLocation
 						// )
-						client_snake.body = server_snake.body; // naive implementation -- could just send head and tail
-						client_snake.tailIndex = server_snake.tailIndex;
+						client_snake.body = server_snake.body // naive implementation -- could just send head and tail
+						client_snake.tailIndex = server_snake.tailIndex
 						// client_snake.socketid = server_snake.socketid
 					}
 				}
-			});
-		});
+			})
+		})
 
 		socket.on("remove", (data) => {
 			data.IDs.forEach((socketid) => {
-				delete Snake.player_list[socketid];
-			});
-		});
+				delete Snake.player_list[socketid]
+			})
+		})
 
 		// naive implementation -- need to have state for food
 		socket.on("food", (data) => {
-			this.food = data.food; // acting as state for food
-		});
+			this.food = data.food // acting as state for food
+		})
+
+		socket.on("dead", () => {
+			this.snakeIsDead = true
+		})
 	}
 
 	startRendering() {
-		const fps = 60;
-		const delayBetweenFramesInMs = 1000 / fps;
+		const fps = 60
+		const delayBetweenFramesInMs = 1000 / fps
 		setInterval(() => {
-			waitingText.innerText = countdownText;
-			if (countdownText <= 1) {
-				waitingText.style.display = "None";
-				waitingBox.style.display = "None";
-				gameMap.style.display = "grid";
+			waitingText.innerText = countdownText
+			if (this.snakeIsDead) {
+				waitingText.innerText = "PATHETIC"
+				back2menuBtn.style.display = "block"
+				waitingBox.style.opacity = "80%"
+				waitingText.style.marginTop = "35%"
+
+				// socket.emit("dead_ack")
+			} else {
+				if (countdownText <= 1) {
+					// waitingBox.style.display = "None"
+					gameMap.style.display = "grid"
+					gameMap.style.zIndex = 0
+					//still didnt do it properly
+					// clock.style.display = "block"
+					waitingBox.style.display = "block"
+					waitingBox.style.zIndex = 1
+					waitingText.style.fontSize = "10vmin"
+					waitingText.style.textAlign = "center"
+					waitingText.style.marginTop = "45%"
+					waitingBox.style.opacity = "30%"
+					waitingText.style.color = "#892be28c"
+					waitingText.innerText = this.timeTillDeath
+				}
 			}
-			drawScoreBoard(Snake.player_list);
-			document.getElementById("game-map").innerHTML = "";
-			drawEverySnake(Snake.player_list);
-			foodDraw(this.food);
-		}, delayBetweenFramesInMs);
+
+			drawScoreBoard(Snake.player_list)
+			document.getElementById("game-map").innerHTML = ""
+			drawEverySnake(Snake.player_list)
+			foodDraw(this.food)
+		}, delayBetweenFramesInMs)
 	}
 
 	keyDownHandler(e) {
-		var key = e.key || e.keyCode;
+		var key = e.key || e.keyCode
 		if (key === 68 || key == "d" || key == "ArrowRight") {
 			//d
 			if (this.direction != "left") {
-				this.direction = "right";
-				socket.emit("keyDown", { dir: this.direction });
+				this.direction = "right"
+				socket.emit("keyDown", { dir: this.direction })
 			}
 		} else if (key === 83 || key == "s" || key == "ArrowDown") {
 			//s
 			if (this.direction != "up") {
-				this.direction = "down";
-				socket.emit("keyDown", { dir: this.direction });
+				this.direction = "down"
+				socket.emit("keyDown", { dir: this.direction })
 			}
 		} else if (key === 65 || key == "a" || key == "ArrowLeft") {
 			//a
 			if (this.direction != "right") {
-				this.direction = "left";
-				socket.emit("keyDown", { dir: this.direction });
+				this.direction = "left"
+				socket.emit("keyDown", { dir: this.direction })
 			}
 		} else if (key === 87 || key == "w" || key == "ArrowUp") {
 			// w
 			if (this.direction != "down") {
-				this.direction = "up";
-				socket.emit("keyDown", { dir: this.direction });
+				this.direction = "up"
+				socket.emit("keyDown", { dir: this.direction })
 			}
 		}
 	}
-
-	// 	setInputDelay(){
-	// 		this.canInteract = false
-	// 		setTimeout(()=>{
-	// 			this.canInteract = true
-	// 		},this.delay)
-	// 	}
 }
--- views/src/index.js
+++ views/src/index.js
@@ -1,31 +1,83 @@
-import Game from "./game.js";
+import Game from "./game.js"
 
-const loginbtn = document.getElementById("loginbtn");
-const registerbtn = document.getElementById("registerbtn");
-const signinModal = document.getElementById("signinmodal");
-const menuModal = document.getElementById("menuModal");
-const joinGameBtn = document.getElementById("joinGameBtn");
-const leaderBoardBtn = document.getElementById("leaderBoardBtn");
-const gameMap = document.getElementById("game-map");
-const scoreBoard = document.getElementById("scoreboard");
-const waitingBox = document.getElementById("waiting-box");
-const waitingText = document.getElementById("waiting-text");
+const loginbtn = document.getElementById("loginbtn")
+const registerbtn = document.getElementById("registerbtn")
+const signinModal = document.getElementById("signin-modal")
+const menuModal = document.getElementById("menuModal")
+const joinGameBtn = document.getElementById("joinGameBtn")
+const leaderBoardBtn = document.getElementById("leaderBoardBtn")
+const gameMap = document.getElementById("game-map")
+const scoreBoard = document.getElementById("scoreboard")
+const waitingBox = document.getElementById("waiting-box")
+const container = document.getElementById("container")
+const back2menuBtn = document.getElementById("back2menuBtn")
+const waitingText = document.getElementById("waiting-text")
 
-loginbtn.addEventListener("click", mainMenu);
-registerbtn.addEventListener("click", mainMenu);
-joinGameBtn.addEventListener("click", tryJoiningGame);
+loginbtn.addEventListener("click", mainMenu)
+registerbtn.addEventListener("click", mainMenu)
+joinGameBtn.addEventListener("click", tryJoiningGame)
+back2menuBtn.addEventListener("click", goBackToMenu)
 
 function mainMenu() {
-	signinModal.style.display = "None";
-	menuModal.style.display = "block";
+	signinModal.style.display = "None"
+	menuModal.style.display = "block"
+}
+
+function goBackToMenu() {
+	/** Removes CSS for:
+	 * - Game map
+	 * - Waiting box
+	 * - The container holding game map and waiting box
+	 * - The scoreboard
+	 */
+	// gameMap.style.display = "None"
+	// waitingBox.style.display = "None"
+	// container.style.display = "None"
+	// scoreBoard.style.display = "None"
+	// /** Removes the inner HTML of:
+	//  * - Game map
+	//  * - Waiting box (holding menubtn and waitingText)
+	//  * - Scoreboard
+	//  */
+	// gameMap.innerHTML = ""
+	// scoreBoard.innerHTML = ""
+	// waitingBox.innerHTML = ""
+	// waitingText.innerHTML = ""
+	// /** Unhides the menu modal */
+	// menuModal.style.display = "block"
+	location.reload(true)
+}
+
+/** TODO: Make waitingText go away and then come back */
+
+function resetGame() {
+	/** Resets the scoreboard */
+	scoreBoard.style.display = "block"
+	let scoreboardTable = document.createElement("table")
+	scoreBoard.append(scoreboardTable)
+
+	/** Resets the menu button */
+	let newBack2menuBtn = document.createElement("button")
+	newBack2menuBtn.id = "back2menuBtn"
+	newBack2menuBtn.innerText = "Main Menu"
+	waitingBox.append(newBack2menuBtn)
+
+	/** Resets the waiting text */
+	let newWaitingText = document.createElement("h1")
+	newWaitingText.id = "waiting-text"
+	waitingBox.append(waitingText)
+
+	gameMap.style.display = "grid"
 }
 
 function tryJoiningGame() {
-	menuModal.style.display = "none";
-	waitingBox.style.display = "flex";
+	// resetGame()
+	container.style.display = "flex"
+	menuModal.style.display = "none"
+	waitingBox.style.display = "flex"
 	// gameMap.style.display = "grid";
-	scoreBoard.style.display = "block";
-	new Game(); // this is the thing that makes them join the socket_list (shows 37eb8237e3AAAA connected)
+	scoreBoard.style.display = "block"
+	new Game() // this is the thing that makes them join the socket_list (shows 37eb8237e3AAAA connected)
 }
 
 /**
