893b965f5701794afbd72fba8a06e7aef1dba6b1
mma37 <mma37@hw.ac.uk>
2020-11-26 11:31:43 +0400

    Added Comments to the Controllers File.

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -3,6 +3,11 @@ const bcrypt = require("bcrypt")
 const saltRounds = 10
 /** setTimeouts allows time for database querying */
 
+/**
+ * Check if the clients credintials are correct. Compare their hashed password inside the database with the input they entered.
+ * @param {String} data the username and the password.
+ * @param {Function} callback will execute on a succesful query
+ */
 let isValidLoginAttempt = function (data, cb) {
 	setTimeout(function () {
 		SQL.getHashedPasswordByUser(data.username, function (hashedPassword) {
@@ -15,13 +20,22 @@ let isValidLoginAttempt = function (data, cb) {
 	}, 100)
 }
 
+/**
+ * Check if the username entered by the user to register is taken by another user or not.
+ * @param {String} data the username of the player.
+ * @param {Function} callback will execute on a succesful query
+ */
 let isUsernameTaken = function (data, cb) {
 	setTimeout(function () {
 		SQL.getUser(data.username, cb)
 	}, 100)
 }
 
-// Now we can store the password hash in db.
+/**
+ * Adding the hashed password and the username of the player when he registers.
+ * @param {String} data both username and password.
+ * @param {Function} callback will execute on a succesful query
+ */
 let addUser = function (data, cb) {
 	setTimeout(function () {
 		bcrypt.hash(data.password, saltRounds, (err, hashedPass) => {
--- controllers/LeaderboardController.js
+++ controllers/LeaderboardController.js
@@ -2,18 +2,33 @@ const SQL = require("../DB/queries.js")
 
 /** setTimeouts allows time for database querying */
 
+/**
+ * Executes the SQL qurey, to get the information about the leaderboard.
+ * @param {Function} callback will execute on a succesful query
+ */
 let getLeaderboardData = function (cb) {
 	setTimeout(function () {
 		SQL.getOrderedLeaderboard(cb)
 	}, 100)
 }
 
+/**
+ * Send back new information to the database and excutes the SQL qurey.
+ * @param {String} user the username of the player.
+ * @param {String} wins the wins of the player.
+ * @param {Function} callback will execute on a succesful query
+ */
 let updateLeaderboardData = function (user, wins, cb) {
 	setTimeout(function () {
 		SQL.updateLeaderboard(user, wins, cb)
 	}, 100)
 }
 
+/**
+ * Executes the SQL qurey, to get the wins of the player needed.
+ * @param {String} username the username of the player.
+ * @param {Function} callback will execute on a succesful query
+ */
 let getWinsbyPlayer = function (username, cb) {
 	setTimeout(function () {
 		SQL.getWinsbyPlayer(username, cb)
